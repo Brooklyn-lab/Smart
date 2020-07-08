73b37f067978b172684f17f2b2f86e3d6dd93003
Пользователь <stepanyukilja@gmail.com>
2020-07-09 00:31:11 +0300

    add header

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-jQuery(document).ready(function ($) {
- 
-});
+(function ($) {
+   let textMore = $(".more");
+   let btn = $("#buttonNo");
+
+   $(btn).on("click", function () {
+      console.log(1);
+      console.log(alert(1));
+      $(this).toggleClass("active");
+      textMore.toggleClass("active");
+
+   });
+
+})(jQuery);
