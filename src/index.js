c45610edcae68c71f817ab24bcc209c60631acab
Пользователь <stepanyukilja@gmail.com>
2020-07-11 00:25:52 +0300

    add slider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 (function ($) {
    let textMore = $(".more");
-   let btn = $("#buttonNo");
+   let btn = $(".header__arrow");
+   let arrowLeft = $(".left");
+   let arrowRight = $(".right");
+
+   $(arrowLeft).on("click", function () {
+   });
+
+   $(arrowRight).on("click", function () {
+   });
 
    $(btn).on("click", function () {
-      console.log(1);
-      console.log(alert(1));
-      $(this).toggleClass("active");
       textMore.toggleClass("active");
-
+      $(this).toggleClass("active");
    });
 
 })(jQuery);
+
+$(document).ready(function () {
+   $(".slider").slick( {
+      slidesToShow: 1,
+      adaptiveHeight: true,
+      speed: 1000
+   });
+
+   $(".slider").on('afterChange', function(event, slick, currentSlide){
+      $("#cp").text(currentSlide + 1);
+   });
+
+});
